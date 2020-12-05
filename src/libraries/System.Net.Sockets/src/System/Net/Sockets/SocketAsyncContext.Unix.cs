5e88630f827b4f7a4d049f4575c3cc55399262a7
Geoffrey Kizer <geoffrek@windows.microsoft.com>
2020-12-05 15:14:30 -0800

    disable trace

--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncContext.Unix.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncContext.Unix.cs
@@ -1508,7 +1508,7 @@ namespace System.Net.Sockets
 
 
         //private static readonly bool TraceEnabled = Environment.GetEnvironmentVariable("SOCKETTRACE") == "1";
-        private const bool TraceEnabled = true;
+        private const bool TraceEnabled = false;
 
         [System.Runtime.InteropServices.DllImport("libc")] private static extern int printf(string format, string arg);
 
